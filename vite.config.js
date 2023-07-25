15e38e7cfbe9521bf43a30bf105dba40ec1aed17
david <david.antonio12@hotmail.com>
2023-07-25 15:31:05 -0500

    Update base path in vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/h3lix/',
+  base: '/',
   plugins: [vue()],
-  define: {
-    'process.env': {},
-  },
 })
