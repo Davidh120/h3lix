cfd8fefb4a51f0f406ee9b4d2c17dd80f4c84d64
david <david.antonio12@hotmail.com>
2023-07-25 11:32:44 -0500

    Retry: Refine API Key retrieval using secrets

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
   base: '/h3lix/',
   plugins: [vue()],
   define: {
-    'process.env': {
-      VITE_API_KEY: JSON.stringify(process.env.VITE_API_KEY || ''),
-    },
+    'process.env': {},
   },
 })
